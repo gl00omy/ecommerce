0b3a9079b2598d40c34d74a6f8b09a525270f8ec
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2023-05-31 11:43:56 +0200

    Hai Duy Huynh - Missing CSRF Token Fix

--- store/views.py
+++ store/views.py
@@ -37,6 +37,7 @@ def cart(request):
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
+
 def checkout(request):
 	if request.user.is_authenticated:
 		customer = request.user.customer
@@ -77,6 +78,9 @@ def updateItem(request):
 
 	return JsonResponse('Item was added', safe=False)
 
+#from django.views.decorators.csrf import csrf_exempt
+
+#@csrf_exempt
 def processOrder(request):
 	transaction_id = datetime.datetime.now().timestamp()
 	data = json.loads(request.body)
