11ccf56f62805c39769278f3fb3b9a4a765a4d9f
Hai Duy Huynh <huynhhaiduy17051999@gmail.com>
2023-06-05 03:31:36 +0200

    Hai Duy Huynh - Paypal Payments

--- store/views.py
+++ store/views.py
@@ -2,22 +2,24 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
-
 from .models import * 
-from . utils import cookieCart, cartData, guestOrder
+from .utils import cookieCart, cartData, guestOrder
 
 def store(request):
-
 	data = cartData(request)
+
 	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
 
 	products = Product.objects.all()
 	context = {'products':products, 'cartItems':cartItems}
 	return render(request, 'store/store.html', context)
 
-def cart(request):
 
+def cart(request):
 	data = cartData(request)
+
 	cartItems = data['cartItems']
 	order = data['order']
 	items = data['items']
@@ -25,10 +27,9 @@ def cart(request):
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
-
 def checkout(request):
-	#Create empty cart for now for non-logged in user
 	data = cartData(request)
+	
 	cartItems = data['cartItems']
 	order = data['order']
 	items = data['items']
@@ -61,9 +62,6 @@ def updateItem(request):
 
 	return JsonResponse('Item was added', safe=False)
 
-#from django.views.decorators.csrf import csrf_exempt
-
-#@csrf_exempt
 def processOrder(request):
 	transaction_id = datetime.datetime.now().timestamp()
 	data = json.loads(request.body)
@@ -71,13 +69,9 @@ def processOrder(request):
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		
-	
 	else:
 		customer, order = guestOrder(request, data)
 
-	
-	
 	total = float(data['form']['total'])
 	order.transaction_id = transaction_id
 
@@ -87,12 +81,12 @@ def processOrder(request):
 
 	if order.shipping == True:
 		ShippingAddress.objects.create(
-			customer=customer,
-			order=order,
-			address=data['shipping']['address'],
-			city=data['shipping']['city'],
-			state=data['shipping']['state'],
-			zipcode=data['shipping']['zipcode'],
-			)
-	
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
 	return JsonResponse('Payment submitted..', safe=False)
